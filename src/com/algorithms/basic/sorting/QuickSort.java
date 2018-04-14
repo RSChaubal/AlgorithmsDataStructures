891590ee6c5399147ffc5f5cc709d027dd7bf1e0
Chaubal <Rushikesh.Chaubal@spglobal.com>
2018-04-15 00:25:55 +0530

    Implemented the quick sort algorithm.
    Signed-off-by: Chaubal <Rushikesh.Chaubal@spglobal.com>

--- src/com/algorithms/basic/sorting/QuickSort.java
+++ src/com/algorithms/basic/sorting/QuickSort.java
@@ -1,34 +1,23 @@
 package com.algorithms.basic.sorting;
 
 /**
- * Quick sort - If we want to arrange an array in ascending order then 
- * it functions by finding the smallest element and exchanging it with the 
- * first element, and repeat the following process on the sub-arrays till 
- * the whole list is sorted.
+ * Quick sort - It works by picking up a pivot value and partioning the array.
+ * Then all the values before the pivot are put to its left and all the values
+ * above the pivot are put to its right. Then again partions are performed on 
+ * the left and the right of the pivot elements. This process is repeated over
+ * and over again till all the elements are sorted.
  *
  * It is a divide and concur algorithm.
  *
- * Has best case performance of O(n), worst and average case performance of O(n2). 
- * For a sorted list, one should terminate the program checking if no exchanges are
- * required. Then in this case, the program would require only one pass and 
- * max (n-1) comparisons would be required in that single pass. So in that case, we 
- * would be able to say that the complexity is of order of O(n).
+ * Has worst case performance of O(n2), which is, if the array is large
+ * pathologically sorted data set. 
+ * 
+ * Its average case and best case performance is O(nlogn), which makes
+ * it appropriate for large as well as small data sets. 
  * 
  * Space required is O(n) as operations are performed directly on the array
  * and no additional space is used.
  * 
- * Not appropriate for large unsorted data sets. Ok for small & nearly sorted sets.
- * 
- * Performs better than selection sort if the array is mostly sorted,  
- * because the correct element will fall into its place in the first 
- * iteration itself.
- * 
- * Used if comparisions are really cheap and swaps are expensive. It will 
- * perform better when minimal space is available. So in this case it 
- * will perform better than merge sort.
- * 
- * Sorting occurs right to left -> max to min.
- *
  * @author rushikesh_chaubal.
  *         Created Apr 10, 2018.
  */
@@ -37,10 +26,11 @@ public class QuickSort {
 	@SuppressWarnings("javadoc")
 	public static void main (String [] args) {
 		
-		int [] arr = {1, 4, 7, 3, 35, 25, 100, 74} ;
+		// int [] arr = {1, 4, 7, 3, 35, 25, 100, 74} ;
+		int [] arr = {10, 11, 44, 6, 2, 44, 70, 25} ;
 		printArray (arr, "original") ;
 		
-		arr = quickSort (arr) ;
+		arr = quickSort (arr, 0, arr.length - 1) ;
 		printArray (arr, "sorted") ;
 	}
 	
@@ -65,20 +55,48 @@ public class QuickSort {
 	 * Method for sorting the passed in array using the quick sort algorithm.
 	 *
 	 * @param arr
+	 * @param low 
+	 * @param high 
 	 * @return
 	 */
-	protected static int[] quickSort (int [] arr) {
+	protected static int[] quickSort (int [] arr, int low, int high) {
 		
-		for (int i = 1; i < arr.length; i++) {
-			int key = arr[i] ;
-			for (int j = i-1; j >= 0 && arr[j] > key; j--) {
-				arr[j+1] = arr[j+1] + arr[j] ;
-				arr[j] = arr[j+1] - arr[j] ;
-				arr[j+1] = arr[j+1] - arr[j] ; 
-			}
-			printArray(arr, "sorting") ;
+		if (low < high) {
+			int pivot = partition (arr, low, high) ;
+			
+			quickSort (arr, pivot+1, high) ;
+			quickSort (arr, low, pivot-1) ;
 		}
+			
 		return arr ;
+	}
+	
+	
+	/**
+	 * Method for partitioning the array based on the pivot element.
+	 *
+	 * @param arr
+	 * @param low
+	 * @param high
+	 * @return
+	 */
+	protected static int partition (int [] arr, int low, int high) {
+		
+		int i = low - 1 ;
+		
+		for (int j = low; j < high; j++) {
+			if (arr[j] < arr[high] && arr[++i] != arr[j]) {
+				arr[j] = arr[j] + arr[i] ;
+				arr[i] = arr[j] - arr[i] ;
+				arr[j] = arr[j] - arr[i] ;
+			}
+		}
+		
+		arr[high] = arr[high] + arr[i+1] ;
+		arr[i+1] = arr[high] - arr[i+1] ;
+		arr[high] = arr[high] - arr[i+1] ;
+		
+		return i+1 ;
 	}
 
 }
